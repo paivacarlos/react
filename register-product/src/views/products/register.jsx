6429cee0544d0167b5be74bc6c54b9d0cf5ca87d
digifred-teste <cp376897@gmail.com>
2021-02-13 16:36:56 -0300

    feat: add layout products-register

--- register-product/src/views/products/register.jsx
+++ register-product/src/views/products/register.jsx
@@ -5,14 +5,67 @@ export default class ProductRegister extends React.Component {
 	render(){
 		return(
 			<div className="card">
+
 				<div className="card-header">
 					Cadstro de Produto
 				</div>
+
 				<div className="card-body">
 
-				</div>
-			</div>
-		)
-	}
+					<div className="row">
+						<div className="col-md-6">
+							<div className="form-group">
+								<label>Nome do Produto: *</label>
+								<input type="text" className="form-control"/>
+							</div>
+						</div>
 
+						<div className="col-md-6">
+							<div className="form-group">
+									<label>SKU: *</label>
+									<input type="text" className="form-control"/>
+							</div>	
+					    </div>
+				    </div>
+
+					<div className="row">
+						<div className="col-md-12">
+							<div className="form-group">
+								<label>Descrição: *</label>
+								<textarea className="form-control"/>
+							</div>
+						</div>
+				    </div>                    
+
+					<div className="row">
+						<div className="col-md-6">
+							<div className="form-group">
+								<label>Preço: *</label>
+								<input type="text" className="form-control"/>
+							</div>
+						</div>
+
+						<div className="col-md-6">
+							<div className="form-group">
+									<label>Fornecedor: *</label>
+									<input type="text" className="form-control"/>
+							</div>	
+					    </div>
+				    </div> 
+
+                    <div className="row">
+                        <div className="col-md-1">
+                            <button className="btn btn-success">Salvar</button>
+                        </div>
+
+                        <div className="col-md-1">
+                            <button className="btn btn-primary">Limpar</button>
+                        </div>
+                    </div>                   
+
+			    </div>
+                
+		    </div>
+	)
+  }
 }

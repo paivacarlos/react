d0f88c80b7cdcd5beff4ac78e12e84d25ee58c86
digifred-teste <cp376897@gmail.com>
2021-02-13 18:16:11 -0300

    feat: add productService

---
+++ register-product/src/app/productService.js
@@ -0,0 +1,20 @@
+
+const PRODUCTS = '_PRODUCTS'
+
+export default class ProductService {
+    
+    save = (product) => {
+        let products = localStorage.getItem(PRODUCTS)
+        
+        if(!products){
+            products = []
+        }else{
+            products = JSON.parse(products)
+        }
+
+        products.push(product)
+
+        localStorage.setItem(PRODUCTS, JSON.stringify(products))
+    }        
+
+}

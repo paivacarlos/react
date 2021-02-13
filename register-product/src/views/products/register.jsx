3c65ca84ba761da6164419916eb521756feb4e70
digifred-teste <cp376897@gmail.com>
2021-02-13 15:30:20 -0300

    feat: add register component

---
+++ register-product/src/views/products/register.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class ProductRegister extends React.Component {
+
+	render(){
+		return(
+			<div className="card">
+				<div className="card-header">
+					Cadstro de Produto
+				</div>
+				<div className="card-body">
+
+				</div>
+			</div>
+		)
+	}
+
+}

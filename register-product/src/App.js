dda552c1be9421c4f3013b1fca0eaa03c9197dda
digifred-teste <cp376897@gmail.com>
2021-02-13 21:10:55 -0300

    feat: add HashRouter for better organization among the files

--- register-product/src/App.js
+++ register-product/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Navbar from './components/navbar'
 import Routes from './routes'
 
+import { HashRouter } from 'react-router-dom'
+
 function App() {
   return (
-    <>
+    <HashRouter>
       <div className="container">
         <Navbar />
         <Routes />
       </div>
-    </>    
+    </HashRouter>    
   );
 }
 
